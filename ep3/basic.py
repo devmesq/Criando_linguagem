74e3a09bb856bb008ee7a0af301af29bb192ee11
sandyrodriguesjs <122131843+sandyrodriguesjs@users.noreply.github.com>
2023-07-09 22:09:46 -0300

    Update tipos de erros

--- ep3/basic.py
+++ ep3/basic.py
@@ -28,36 +28,37 @@ class Erro:
 		resultado += '\n\n' + string_com_setas(self.posicao_inicial.ftxt, self.posicao_inicial, self.posicao_final)
 		return resultado
 
-class IllegalCharError(Error):
-	def __init__(self, pos_start, pos_end, details):
-		super().__init__(pos_start, pos_end, 'Illegal Character', details)
+	class ErroCaractereIlegal(Erro):
+	def __init__(self, posicao_inicial, posicao_final, detalhes):
+		super().__init__(posicao_inicial, posicao_final, 'Caractere Ilegal', detalhes)
 
-class InvalidSyntaxError(Error):
-	def __init__(self, pos_start, pos_end, details=''):
-		super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+	class ErroSintaxeInvalida(Erro):
+	def __init__(self, posicao_inicial, posicao_final, detalhes=''):
+		super().__init__(posicao_inicial, posicao_final, 'Sintaxe Inválida', detalhes)
 
-class RTError(Error):
-	def __init__(self, pos_start, pos_end, details, context):
-		super().__init__(pos_start, pos_end, 'Runtime Error', details)
-		self.context = context
+	class ErroTempoExecucao(Erro):
+	def __init__(self, posicao_inicial, posicao_final, detalhes, contexto):
+		super().__init__(posicao_inicial, posicao_final, 'Erro de Tempo de Execução', detalhes)
+		self.contexto = contexto
 
-	def as_string(self):
-		result  = self.generate_traceback()
-		result += f'{self.error_name}: {self.details}'
-		result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
-		return result
+	def como_string(self):
+		resultado  = self.gerar_rastreamento()
+		resultado += f'{self.nome_erro}: {self.detalhes}'
+		resultado += '\n\n' + string_com_setas(self.posicao_inicial.ftxt, self.posicao_inicial, self.posicao_final)
+		return resultado
 
-	def generate_traceback(self):
-		result = ''
-		pos = self.pos_start
-		ctx = self.context
+	def gerar_rastreamento(self):
+		resultado = ''
+		posicao = self.posicao_inicial
+		ctx = self.contexto
 
 		while ctx:
-			result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
-			pos = ctx.parent_entry_pos
-			ctx = ctx.parent
+			resultado = f'  Arquivo {posicao.fn}, linha {str(posicao.ln + 1)}, em {ctx.nome_exibicao}\n' + resultado
+			posicao = ctx.posicao_entrada_pai
+			ctx = ctx.pai
 
-		return 'Traceback (most recent call last):\n' + result
+		return 'Rastreamento (chamada mais recente por último):\n' + resultado
+
 
 #######################################
 # POSITION
