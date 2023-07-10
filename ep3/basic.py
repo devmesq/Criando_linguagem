0b7d0d262f94bc24c14752a81d5952776d206f4a
sandyrodriguesjs <122131843+sandyrodriguesjs@users.noreply.github.com>
2023-07-09 22:16:04 -0300

    Update Lexer

--- ep3/basic.py
+++ ep3/basic.py
@@ -126,70 +126,72 @@ class Token:
 #######################################
 
 class Lexer:
-	def __init__(self, fn, text):
+	def __init__(self, fn, texto):
 		self.fn = fn
-		self.text = text
-		self.pos = Position(-1, 0, -1, fn, text)
-		self.current_char = None
-		self.advance()
+		self.texto = texto
+		self.pos = Posicao(-1, 0, -1, fn, texto)
+		self.caractere_atual = None
+		self.avancar()
 	
-	def advance(self):
-		self.pos.advance(self.current_char)
-		self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+	def avancar(self):
+		self.pos.avancar(self.caractere_atual)
+		self.caractere_atual = self.texto[self.pos.idx] if self.pos.idx < len(self.texto) else None
 
-	def make_tokens(self):
+	def criar_tokens(self):
 		tokens = []
 
-		while self.current_char != None:
-			if self.current_char in ' \t':
-				self.advance()
-			elif self.current_char in DIGITS:
-				tokens.append(self.make_number())
-			elif self.current_char == '+':
-				tokens.append(Token(TT_PLUS, pos_start=self.pos))
-				self.advance()
-			elif self.current_char == '-':
-				tokens.append(Token(TT_MINUS, pos_start=self.pos))
-				self.advance()
-			elif self.current_char == '*':
-				tokens.append(Token(TT_MUL, pos_start=self.pos))
-				self.advance()
-			elif self.current_char == '/':
-				tokens.append(Token(TT_DIV, pos_start=self.pos))
-				self.advance()
-			elif self.current_char == '(':
-				tokens.append(Token(TT_LPAREN, pos_start=self.pos))
-				self.advance()
-			elif self.current_char == ')':
-				tokens.append(Token(TT_RPAREN, pos_start=self.pos))
-				self.advance()
+		while self.caractere_atual is not None:
+			if self.caractere_atual in ' \t':
+				self.avancar()
+			elif self.caractere_atual in DIGITOS:
+				tokens.append(self.criar_numero())
+			elif self.caractere_atual == '+':
+				tokens.append(Token(TT_MAIS, pos_start=self.pos))
+				self.avancar()
+			elif self.caractere_atual == '-':
+				tokens.append(Token(TT_MENOS, pos_start=self.pos))
+				self.avancar()
+			elif self.caractere_atual == '*':
+				tokens.append(Token(TT_MULTIPLICACAO, pos_start=self.pos))
+				self.avancar()
+			elif self.caractere_atual == '/':
+				tokens.append(Token(TT_DIVISAO, pos_start=self.pos))
+				self.avancar()
+			elif self.caractere_atual == '(':
+				tokens.append(Token(TT_ABRE_PARENTESES, pos_start=self.pos))
+				self.avancar()
+			elif self.caractere_atual == ')':
+				tokens.append(Token(TT_FECHA_PARENTESES, pos_start=self.pos))
+				self.avancar()
 			else:
-				pos_start = self.pos.copy()
-				char = self.current_char
-				self.advance()
-				return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+				pos_start = self.pos.copiar()
+				char = self.caractere_atual
+				self.avancar()
+				return [], ErroCaractereIlegal(pos_start, self.pos, "'" + char + "'")
 
-		tokens.append(Token(TT_EOF, pos_start=self.pos))
+		tokens.append(Token(TT_FIM_DE_ARQUIVO, pos_start=self.pos))
 		return tokens, None
 
-	def make_number(self):
+	def criar_numero(self):
 		num_str = ''
-		dot_count = 0
-		pos_start = self.pos.copy()
+		contagem_ponto = 0
+		pos_start = self.pos.copiar()
 
-		while self.current_char != None and self.current_char in DIGITS + '.':
-			if self.current_char == '.':
-				if dot_count == 1: break
-				dot_count += 1
+		while self.caractere_atual is not None and self.caractere_atual in DIGITOS + '.':
+			if self.caractere_atual == '.':
+				if contagem_ponto == 1:
+					break
+				contagem_ponto += 1
 				num_str += '.'
 			else:
-				num_str += self.current_char
-			self.advance()
+				num_str += self.caractere_atual
+			self.avancar()
 
-		if dot_count == 0:
+		if contagem_ponto == 0:
 			return Token(TT_INT, int(num_str), pos_start, self.pos)
 		else:
 			return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
+
 
 #######################################
 # NODES
